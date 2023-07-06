2dcd8516a2b6b35dfc7a76cc81940a79ebb93c74
Denis.S <denis.s@MacBook-Pro-3.local>
2023-07-06 13:47:08 +0300

    exponential short value

--- src/utils/helpers/token-uri.ts
+++ src/utils/helpers/token-uri.ts
@@ -3,6 +3,16 @@ import { AssetType, DNALength } from '../enums';
 
 export function generateDNA(assetType: AssetType): string {
   const length = DNALength[assetType];
-  const tokenURIBuffer = getRandomValues(new Uint32Array(length));
+  const newIntArray = new Uint32Array(length);
+  const tokenURIBuffer = getRandomValues(newIntArray);
+  for (let i = length - 8; i < length; i++) {
+    tokenURIBuffer[i] = getExponentialShortValue();
+  }
   return Buffer.from(tokenURIBuffer.buffer).toString('hex');
+}
+
+function getExponentialShortValue() {
+  const firstValue = Math.floor(Math.log(Math.random()) * -7057);
+  const secondValue = Math.floor(Math.log(Math.random()) * -7057);
+  return Number(`${firstValue}${secondValue}`);
 }
