98142a22df053af415318ecfc36339498de3409f
Denis.S <denis.s@MacBook-Pro-3.local>
2023-07-07 14:18:50 +0300

    added gaussian short value

--- src/utils/helpers/token-uri.ts
+++ src/utils/helpers/token-uri.ts
@@ -13,10 +13,16 @@ export function generateDNA(assetType: AssetType): string {
       break;
   }
   let dna = '';
-  for (let i = 0; i < length - countOfBytes; i++) {
+  for (let i = 0; i < length - countOfBytes - countOfBytes; i++) {
     const randomInt = getRandom32BitInt();
     const hexValue = randomInt.toString(16).padStart(8, '0');
     dna += hexValue;
+  }
+
+  for (let i = 0; i < countOfBytes; i++) {
+    const result = getGaussianShortValue();
+
+    dna += result;
   }
 
   for (let i = 0; i < countOfBytes; i++) {
@@ -39,4 +45,28 @@ function getExponentialShortValue() {
 
 function getRandom32BitInt() {
   return Math.floor(Math.random() * 4294967296); // Generate a random integer between 0 and 2^32-1
+}
+
+function getGaussianRandom(min, max, skew): number {
+  let u = 0,
+    v = 0;
+  while (u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+  while (v === 0) v = Math.random();
+  let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+
+  num = num / 10.0 + 0.5; // Translate to 0 -> 1
+  if (num > 1 || num < 0) num = getGaussianRandom(min, max, skew); // resample between 0 and 1 if out of range
+  num = Math.pow(num, skew); // Skew
+  num *= max - min; // Stretch to fill range
+  num += min; // offset to min
+  return num;
+}
+
+function getGaussianShortValue() {
+  const exponentialPart1 = Math.round(getGaussianRandom(0, 65535, 1));
+  const exponentialPart2 = Math.round(getGaussianRandom(0, 65535, 1));
+  const hexValue1 = exponentialPart1.toString(16).padStart(4, '0');
+  const hexValue2 = exponentialPart2.toString(16).padStart(4, '0');
+
+  return hexValue1 + hexValue2;
 }
